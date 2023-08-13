f936beca2166a17eff5bd4ae9ad9e4f17c7c6dc6
Bisharah Estephan <estephan.be@gmail.com>
2023-08-13 20:26:17 +0300

    first commit

---
+++ lang/en/theme_htu_boshdev.php
@@ -0,0 +1,7 @@
+<?php
+
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'HTU BoshDev Theme';
+$string['choosereadme'] = 'Child theme for Boost made by HTU-BoshDev.';
---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'theme_htu_boshdev';
+$plugin->version   = 2021052401;
+$plugin->requires  = 2021051100;
+$plugin->dependencies = ['theme_boost' => 2023042400];
