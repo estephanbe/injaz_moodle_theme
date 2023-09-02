f113b6b1d7fb0b7ec45ae9aa686929ced5700b1d
Bisharah Estephan <estephan.be@gmail.com>
2023-09-03 00:24:18 +0300

    activity icons and updates

--- javascript/custom.js
+++ javascript/custom.js
@@ -6,9 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const url = document.querySelectorAll('li.activity.modtype_url .activitytitle .activityiconcontainer img');
     const hvp = document.querySelectorAll('li.activity.modtype_hvp .activitytitle .activityiconcontainer img');
     const challenge = document.querySelectorAll('li.activity.modtype_quiz .activitytitle .activityiconcontainer img');
-    console.log(url);
-    console.log(hvp);
-    console.log(challenge);
+
     url.forEach(function(element) {
         element.setAttribute('src', M.cfg.wwwroot + '/theme/htu_boshdev/pix/activities_icons/Learn.png');
         element.style.height = '100%';
@@ -35,17 +33,53 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     // Dev
-    const editField = document.querySelectorAll(`[data-name="Age Group"]`);
-    editField.forEach(element => {
-        element.addEventListener('click', function(e){
-            setTimeout(function(){
-                console.log(document.querySelectorAll('.modal'))
-                document.querySelector('.modal').style.display = 'block';
-            }, 3000)
-        });
-    })
+    // const editField = document.querySelectorAll(`[data-name="Age Group"]`);
+    // editField.forEach(element => {
+    //     element.addEventListener('click', function(e){
+    //         setTimeout(function(){
+    //             console.log(document.querySelectorAll('.modal'))
+    //             document.querySelector('.modal').style.display = 'block';
+    //         }, 3000)
+    //     });
+    // })
+    // console.log(editField)
 
-    console.log(editField)
+    const listItems = document.querySelectorAll('#multi_section_tiles > li');
+
+    listItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            const url = document.querySelectorAll('li.activity.modtype_url .activitytitle .activityiconcontainer img');
+            const hvp = document.querySelectorAll('li.activity.modtype_hvp .activitytitle .activityiconcontainer img');
+            const challenge = document.querySelectorAll('li.activity.modtype_quiz .activitytitle .activityiconcontainer img');
+            console.log(url)
+            console.log(hvp)
+            console.log(challenge)
+            url.forEach(function(element) {
+                element.setAttribute('src', M.cfg.wwwroot + '/theme/htu_boshdev/pix/activities_icons/Learn.png');
+                element.style.height = '100%';
+                element.style.width = '100%';
+                element.classList.add('nofilter');
+                element.parentNode.style.padding= '0';
+            });
+
+            hvp.forEach(function(element) {
+                element.setAttribute('src', M.cfg.wwwroot + '/theme/htu_boshdev/pix/activities_icons/Play.png');
+                element.style.height = '100%';
+                element.style.width = '100%';
+                element.classList.add('nofilter');
+                element.parentNode.style.padding= '0';
+            });
+
+            challenge.forEach(function(element) {
+                element.setAttribute('src', M.cfg.wwwroot + '/theme/htu_boshdev/pix/activities_icons/Challenge.png');
+                element.style.height = '100%';
+                element.style.width = '100%';
+                element.classList.add('nofilter');
+                element.parentNode.style.padding= '0';
+            });
+        });
+    });
+
 
 
 });
