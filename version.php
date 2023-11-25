65d1696ff22262f0eaf9a529cab7d7ba0acc8215
Bisharah Estephan <estephan.be@gmail.com>
2023-11-25 23:23:54 +0300

    images added

--- version.php
+++ version.php
@@ -3,6 +3,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_htu_boshdev';
-$plugin->version   = 2021052404;
+$plugin->version   = 2021052405;
 $plugin->requires  = 2021051100;
 $plugin->dependencies = ['theme_boost' => 2023042400];
